13bdf237d85cfe11f16b20944cf104ff62667a4e
inicius <viniciusf84@hotmail.com>
2023-01-08 19:48:27 -0300

    Add Error404 view

---
+++ src/views/Error404/Error404.styled.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+import Bateman from '../../assets/images/bateman.jpg';
+import { centerContent, colors } from '../../styles/variables';
+
+export const ErrorContainer = styled.div`
+	${centerContent()}
+
+	background: url(${Bateman}) no-repeat;
+	background-size: cover;
+	color: ${colors.white};
+	font-size: 2rem;
+	height: 100vh;
+`;
---
+++ src/views/Error404/Error404.tsx
@@ -0,0 +1,9 @@
+import { ErrorContainer } from './Error404.styled';
+
+export default function Error404() {
+	return (
+		<ErrorContainer>
+			<h2>Erro 404</h2>
+		</ErrorContainer>
+	);
+}
