f67634ca765913decb8077439789a6474bfd77ff
inicius <viniciusf84@hotmail.com>
2023-01-09 00:20:59 -0300

    Add HeaderCart

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useContext } from 'react';
+import { useMemo, useContext } from 'react';
 import { faCheck } from '@fortawesome/free-solid-svg-icons';
 
 // components
@@ -10,24 +10,41 @@ import { ShopContext } from '../../contexts/ShopContext';
 
 // styles
 import { CartItem, TotalSection } from './Cart.styled';
+import { FruitProps } from '../../utils/types';
 
-export default function Cart({ size, list, title, price, image, total }) {
+interface CartItemProps {
+	name: string;
+	price: string;
+	image: string;
+	total: number;
+}
+
+interface CartListProps {
+	item: CartProps;
+	quantity: number;
+}
+interface CartProps extends CartItemProps {
+	list: CartListProps[];
+}
+
+export default function Cart({ list, total }: CartProps) {
 	const shopContext = useContext(ShopContext);
 
-	const { setCheckout } = shopContext.actions;
+	const { setCheckout } = shopContext;
 
 	const displayCart = useMemo(() => {
-		return list.map((item) => (
-			<CartItem key={item.item[title]}>
-				<Image src={item.item[image]} alt={item.item[title]} />
+		console.log(list);
+		return list.map((listItem) => (
+			<CartItem key={listItem.item.name}>
+				<Image src={listItem.item.image} alt={listItem.item.name} />
 
 				<div className="text">
-					<h3 className="title">{item.item[title].toUpperCase()}</h3>
+					<h3 className="title">{listItem.item.name.toUpperCase()}</h3>
 
 					<div className="space-between">
-						<span className="quantity">Quantidade: {item.quantity}</span>
+						<span className="quantity">Quantidade: {listItem.quantity}</span>
 
-						<strong className="price">${item.item[price]}</strong>
+						<strong className="price">${listItem.item.price}</strong>
 					</div>
 				</div>
 			</CartItem>
--- src/components/HeaderCart/HeaderCart.tsx
+++ src/components/HeaderCart/HeaderCart.tsx
@@ -1,108 +1,93 @@
-import React, {
-  useState,
-  useEffect,
-  useMemo,
-  useCallback,
-  useRef,
-} from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import { useState, useEffect, useMemo, useCallback, useRef } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 // components
-import Cart from "../Cart";
-import ShadowStyled from "../Shadow";
+import Cart from '../Cart';
+import ShadowStyled from '../Shadow';
 
 // utils
-import GroupArrayByKey from "../../utils/GroupArrayByKey";
-import SmoothScrollTo from "../../utils/SmoothScrollTo";
+import GroupArrayByKey from '../../utils/groupArrayByKey';
+import SmoothScrollTo from '../../utils/smoothScrollTo';
 
 // styles
 import {
-  HeaderCartStyled,
-  HeaderCartButton,
-  CartModal,
-} from "./HeaderCart.styled";
+	HeaderCartStyled,
+	HeaderCartButton,
+	CartModal,
+} from './HeaderCart.styled';
 
 export default function HeaderCart({ items, total }) {
-  const node = useRef();
+	const node = useRef();
 
-  const [isOpen, setIsOpen] = useState(true);
-  const [formattedArray, setFormattedArray] = useState([]);
+	const [isOpen, setIsOpen] = useState(false);
+	const [formattedArray, setFormattedArray] = useState([]);
 
-  const emptyCart = useMemo(() => {
-    if (items.length > 0) {
-      return false;
-    }
+	const emptyCart = useMemo(() => {
+		if (items.length > 0) {
+			return false;
+		}
 
-    return true;
-  }, [items]);
+		return true;
+	}, [items]);
 
-  const smoothScroll = useCallback(() => {
-    SmoothScrollTo("header");
-  }, []);
+	const smoothScroll = useCallback(() => {
+		SmoothScrollTo('header');
+	}, []);
 
-  const handleClick = (e) => {
-    if (node.current.contains(e.target)) {
-      // inside click
-      return;
-    }
-    // outside click
-    setIsOpen(false);
-  };
+	const handleClick = (e) => {
+		if (node.current.contains(e.target)) {
+			// inside click
+			return;
+		}
+		// outside click
+		setIsOpen(false);
+	};
 
-  useEffect(() => {
-    document.addEventListener("mousedown", handleClick);
+	useEffect(() => {
+		document.addEventListener('mousedown', handleClick);
 
-    return () => {
-      document.removeEventListener("mousedown", handleClick);
-    };
-  }, []);
+		return () => {
+			document.removeEventListener('mousedown', handleClick);
+		};
+	}, []);
 
-  useEffect(() => {
-    const grouped = GroupArrayByKey(items, "id");
-    const groupedArray = Object.values(grouped).map((item) => ({
-      item: item[0],
-      quantity: item.length,
-    }));
+	useEffect(() => {
+		const grouped = GroupArrayByKey(items, 'id');
+		const groupedArray = Object.values(grouped).map((item) => ({
+			item: item[0],
+			quantity: item.length,
+		}));
 
-    setFormattedArray(groupedArray);
-  }, [items]);
+		setFormattedArray(groupedArray);
+	}, [items]);
 
-  useEffect(() => {
-    setIsOpen(true);
-    smoothScroll("header");
-  }, [formattedArray]);
+	useEffect(() => {
+		smoothScroll('header');
+	}, [formattedArray]);
 
-  return (
-    <HeaderCartStyled
-      ref={node}
-      onMouseOver={() => setIsOpen(true)}
-      onMouseLeave={() => setIsOpen(false)}
-    >
-      <HeaderCartButton className={isOpen ? "hover" : ""}>
-        <FontAwesomeIcon icon={faShoppingCart} />
-        <span>{items.length}</span>
-      </HeaderCartButton>
+	return (
+		<HeaderCartStyled
+			ref={node}
+			onMouseOver={() => setIsOpen(true)}
+			onMouseLeave={() => setIsOpen(false)}
+		>
+			<HeaderCartButton className={isOpen ? 'hover' : ''}>
+				<FontAwesomeIcon icon={faShoppingCart} />
+				<span>{items.length}</span>
+			</HeaderCartButton>
 
-      {isOpen && (
-        <>
-          <CartModal className={emptyCart ? "empty" : ""}>
-            {emptyCart && <p>Your shop cart is empty.</p>}
+			{isOpen && (
+				<>
+					<CartModal className={emptyCart ? 'empty' : 'ololo'}>
+						{emptyCart && <p>Seu carrinho ainda está vazio.</p>}
 
-            {!emptyCart && (
-              <Cart
-                list={formattedArray}
-                title="name"
-                price="base_experience"
-                image="image"
-                total={total}
-              />
-            )}
-          </CartModal>
+						{!emptyCart && <Cart list={formattedArray} total={total} />}
+					</CartModal>
 
-          <ShadowStyled onClick={() => setIsOpen(false)} />
-        </>
-      )}
-    </HeaderCartStyled>
-  );
+					<ShadowStyled onClick={() => setIsOpen(false)} />
+				</>
+			)}
+		</HeaderCartStyled>
+	);
 }
